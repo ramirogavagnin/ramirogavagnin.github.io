3e134fa3b82f6148f64a726f1765049ff439688c
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2019-12-17 12:13:53 -0300

    Initial commit

---
+++ precache-manifest.74297b240df4c9bcff113120c41ba8f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fb3ff4221e8f831d210a210b89b881",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3c57a39d60ec57f5df",
+    "url": "/static/css/main.3edcf6ee.chunk.css"
+  },
+  {
+    "revision": "de8567001f8a339fd714",
+    "url": "/static/js/2.88713f63.chunk.js"
+  },
+  {
+    "revision": "0b1e8647e82c18f1bbe8b4a59fb3fbb8",
+    "url": "/static/js/2.88713f63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da3c57a39d60ec57f5df",
+    "url": "/static/js/main.81c1693e.chunk.js"
+  },
+  {
+    "revision": "c53c58812cc6fee31dea",
+    "url": "/static/js/runtime-main.055f3c83.js"
+  },
+  {
+    "revision": "7207dc2d6e59cc1bfa8f2cfef2ef5488",
+    "url": "/static/media/404.7207dc2d.png"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "c09073f8c02ce171d1b60c53c0f066c7",
+    "url": "/static/media/home-background.c09073f8.png"
+  },
+  {
+    "revision": "b60bff27834edf2e9e8733143daf5485",
+    "url": "/static/media/vader.b60bff27.png"
+  }
+]);
